910c0fb42340cfddadeb52b2efe61678a0de8222
dimk2 <dimikir14@gmail.com>
2024-03-18 16:37:31 +0100

    last game class update

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,7 @@ public class Main {
 
 
 
-        GameBoard board = new GameBoard(9, 9, 30, 6, 7);
-        board.getTile(6,7).open();
-        System.out.println(((Number) board.getTile(6,7)).getNeighbors());
-        Visualization visualization = new Visualization("🚩","💣","","‍☠️", "0", "");
-
-        visualization.visualize(board);
-        for (Integer[] localTuple: board.getBombCoordinates()) {
-            if (abs(localTuple[0] - 6) <= 1 && abs(localTuple[1] - 7) <= 1) {
-                System.out.print("(" + localTuple[0] + ", " + localTuple[1] + "), ");
-            }
-//
-        }
+       Game game = new Game();
 
 
 
