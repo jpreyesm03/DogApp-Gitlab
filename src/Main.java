0a896e8cae06af94b2418fb2ebaa9665876dcc87
jpreyesm03 <jpreyesm03@gmail.com>
2024-03-18 16:47:22 +0100

    Apparently everything is correct.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,18 @@ import static java.lang.Math.abs;
 public class Main {
     public static void main(String[] args) {
 
+        GameBoard board = new GameBoard(9, 9, 5, 6, 7);
+        board.getTile(6,7).open();
+        System.out.println(((Number) board.getTile(6,7)).getNeighbors());
+        Visualization visualization = new Visualization("🚩","💣","","‍☠️", "0", "");
 
-
-       Game game = new Game();
-
+        visualization.visualize(board);
+        for (Integer[] localTuple: board.getBombCoordinates()) {
+            if (abs(localTuple[0] - 6) <= 1 && abs(localTuple[1] - 7) <= 1) {
+                System.out.print("(" + localTuple[0] + ", " + localTuple[1] + "), ");
+            }
+//
+        }
 
 
     }
